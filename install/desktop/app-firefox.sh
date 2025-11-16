b5dba2b25649b1ca216c5e04e9332383baafdc1c
Kai Nakamura <kaihnakamura@gmail.com>
2025-11-15 20:56:31 -0500

    Remove firefox install

--- install/desktop/app-firefox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Firefox is already installed on Ubuntu by default, but ensure it's set as default browser
-sudo apt install -y firefox
-xdg-settings set default-web-browser firefox.desktop
-
